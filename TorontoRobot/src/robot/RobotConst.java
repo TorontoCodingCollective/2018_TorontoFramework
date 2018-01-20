1c525563777a9149430cf4d9a30a52bb76a019d3
richardmcmullin <richard.e.mcmullin@gmail.com>
2018-01-20 16:28:03 -0500

    Add support in RobotConst for multiple drive bases

--- TorontoRobot/src/robot/RobotConst.java
+++ TorontoRobot/src/robot/RobotConst.java
@@ -1,6 +1,10 @@
 package robot;
 
 public class RobotConst {
+	
+	// The TorontoCodingCollective framework was developed to run on different
+	// robots.
+	public static int robot = 1311;
 
 	public static final boolean INVERTED = true;
 	public static final boolean NOT_INVERTED = false;
@@ -8,17 +12,57 @@ public class RobotConst {
 	//*********************************************************
 	// Drive Constants
 	//*********************************************************
-	public static final boolean RIGHT_MOTOR_ORIENTATION = NOT_INVERTED;
-	public static final boolean LEFT_MOTOR_ORIENTATION = INVERTED;
+	public static final boolean RIGHT_MOTOR_ORIENTATION;
+	public static final boolean LEFT_MOTOR_ORIENTATION;
 	
-	public static final boolean RIGHT_ENCODER_ORIENTATION = NOT_INVERTED;
-	public static final boolean LEFT_ENCODER_ORIENTATION = INVERTED;
+	public static final boolean RIGHT_ENCODER_ORIENTATION;
+	public static final boolean LEFT_ENCODER_ORIENTATION;
 	
-	public static final double MAX_LOW_GEAR_SPEED = 365.0;
-	public static final double MAX_HIGH_GEAR_SPEED = 830.0;
+	public static final double MAX_LOW_GEAR_SPEED;
+	public static final double MAX_HIGH_GEAR_SPEED;
 	
-	public static final double DRIVE_GYRO_PID_KP = .1;
-	public static final double DRIVE_GYRO_PID_KI = 0;
+	public static final double DRIVE_GYRO_PID_KP;
+	public static final double DRIVE_GYRO_PID_KI;
 	
-	public static final double DRIVE_SPEED_PID_KP = 0.75;
+	public static final double DRIVE_SPEED_PID_KP;
+
+	static {
+		
+		switch (robot) {
+
+		case 1311:
+			RIGHT_MOTOR_ORIENTATION = INVERTED;
+			LEFT_MOTOR_ORIENTATION = NOT_INVERTED;
+			
+			RIGHT_ENCODER_ORIENTATION = NOT_INVERTED;
+			LEFT_ENCODER_ORIENTATION = INVERTED;
+			
+			MAX_LOW_GEAR_SPEED = 365.0;    // Encoder counts/sec
+			MAX_HIGH_GEAR_SPEED = 830.0;
+			
+			DRIVE_GYRO_PID_KP = .1;
+			DRIVE_GYRO_PID_KI = 0;
+			
+			DRIVE_SPEED_PID_KP = 0.75;
+			break;
+			
+		case 1321:
+		default:
+			RIGHT_MOTOR_ORIENTATION = NOT_INVERTED;
+			LEFT_MOTOR_ORIENTATION = INVERTED;
+			
+			RIGHT_ENCODER_ORIENTATION = NOT_INVERTED;
+			LEFT_ENCODER_ORIENTATION = INVERTED;
+			
+			MAX_LOW_GEAR_SPEED = 365.0;    // Encoder counts/sec
+			MAX_HIGH_GEAR_SPEED = 830.0;
+			
+			DRIVE_GYRO_PID_KP = .1;
+			DRIVE_GYRO_PID_KI = 0;
+			
+			DRIVE_SPEED_PID_KP = 0.75;
+		
+			break;
+		}
+	}
 }
