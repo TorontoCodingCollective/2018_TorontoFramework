a7cc8beb6efdd5d56eb5b83e491263987bc58339
Rishab Bhatt <rishabswift@gmail.com>
2018-02-01 15:44:29 -0500

    added ultrasonic

---
+++ TorontoRobot/src/robot/commands/drive/DriveToUltrasonicCommand.java
@@ -0,0 +1,76 @@
+package robot.commands.drive;
+
+import com.torontocodingcollective.sensors.ultrasonic.TUltrasonic;
+
+import robot.Robot;
+
+/**
+ *
+ */
+public class DriveToUltrasonicCommand extends DriveDirectionCommand {
+
+	private static final double STOPPING_DISTANCE = 1.0; // Stopping distance in inches
+	
+	private final double stoppingDistance;
+	private final TUltrasonic ultrasonicSensor;
+	private enum Direction {
+			FORWARD, BACKWARD
+	};
+	
+	private Direction direction;
+
+	public DriveToUltrasonicCommand(double angle, double speed, double stoppingDistance, TUltrasonic ultrasonicSensor) {
+		 super( angle, speed, 15, true);
+		 this.ultrasonicSensor = ultrasonicSensor;
+		 this.stoppingDistance = stoppingDistance;
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.chassisSubsystem);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		
+		super.initialize();
+		
+		//Where am I now?
+		double currentDistance = ultrasonicSensor.getDistance();
+		
+		if (currentDistance >= stoppingDistance) {
+			
+			direction = Direction.FORWARD;
+			
+		} else {
+			
+			direction = Direction.BACKWARD;
+		}
+		//Set the direction
+//		super.setDirection(direction);
+	}
+
+	
+	
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		double currentDistance = ultrasonicSensor.getDistance();
+		if (currentDistance <= stoppingDistance + STOPPING_DISTANCE) {
+			System.out.println("Finished distance " + currentDistance);
+			return true;
+		}
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
